61e5fc11a14f1be67e74145eb25c88d0439e261c
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-02-07 10:14:40 +0100

    Base Objects Controller

---
+++ app/controllers/restored_objects_controller.rb
@@ -0,0 +1,22 @@
+class RestoredObjectsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
---
+++ app/helpers/restored_objects_helper.rb
@@ -0,0 +1,2 @@
+module RestoredObjectsHelper
+end
