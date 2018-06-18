3672e62d3b4a34168b85f6bb8d025b1569e55fd9
duxy1996 <durancdr96@gmail.com>
2018-06-18 21:41:04 +0200

    Added redesign

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,15 @@
 class CategoriesController < ApplicationController
 
-
   def index
     @categories = Category.all
     @objects = RestoredObject.all
   end
 
   def show    
-    
+    @categories  = Category.all
+    @objects     = RestoredObject.all
+    @category    = Category.find(params[:id])
+    @category_id = params[:id]
   end
 
 end
