34d0017c57ddc7f0072a50f51515921a0a490e6e
duxy1996 <durancdr96@gmail.com>
2018-06-19 23:03:09 +0200

    Changed views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,18 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.all
-    @objects = RestoredObject.all
+    @categories  = Category.all
+    @objects     = RestoredObject.all
+    half_size    = @categories.count/2+1
+    @first_half  = @categories[0..half_size] 
+    @second_half = @categories[half_size..(@categories.count)]
   end
 
   def show    
     @categories  = Category.all
     @objects     = RestoredObject.all
     @category    = Category.find(params[:id])
-    @category_id = params[:id]
+    @category_id = params[:id]    
   end
 
 end
