413e4f5dc47039ac23082f49f1d8da839923f871
duxy1996 <durancdr96@gmail.com>
2018-07-19 18:40:39 +0200

    Excluded threeJS files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180716182827) do
+ActiveRecord::Schema.define(version: 20180716181427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,12 +63,6 @@ ActiveRecord::Schema.define(version: 20180716182827) do
   end
 
   create_table "materials", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "object_formats", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
