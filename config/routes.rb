5dab3f7237125e183d35fda42dc6abd478dd5d4b
duxy1996 <durancdr96@gmail.com>
2018-11-06 21:54:48 +0100

    Users view implemented

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   resources :categories
   resources :collections
 
+  match 'users/:id' => 'users#show', via: :get
+
   devise_for :users
 
-  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    root 'categories#index'
+  end
 end
