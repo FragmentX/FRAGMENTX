3e019dfcb8e1193c4dcdcddfc0ab9f9261e5f035
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-11-13 20:13:22 +0100

    Redesigned devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  
+
+  match '/logout', to: 'sessions#destroy', via: [:get, :post]
+
   resources :restored_objects
   resources :categories
 
   devise_for :users
-   
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
     root 'categories#index'
