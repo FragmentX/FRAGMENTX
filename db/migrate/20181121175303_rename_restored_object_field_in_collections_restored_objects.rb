1084292633f5f1d50e16f4a24152ec925dfc4c6a
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-11-21 19:48:55 +0100

    Added collections

--- db/migrate/20181121175303_rename_restored_object_field_in_collections_restored_objects.rb
+++
@@ -1,5 +0,0 @@
-class RenameRestoredObjectFieldInCollectionsRestoredObjects < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :collections_restored_objects, :restored_object, :restored_object_id
-  end
-end
