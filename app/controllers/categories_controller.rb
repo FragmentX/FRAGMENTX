95ab636a0eed7880dfc13856739c6f2012e27ebf
duxy1996 <durancdr96@gmail.com>
2018-11-27 22:33:18 +0100

    [Issue #4]-Refactored user dashboard interface

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,29 +1,14 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories  = Category.all
-    @objects     = RestoredObject.all
-    half_size    = @categories.count/2+1
-    @first_half  = @categories[0..half_size]
-    @second_half = @categories[half_size..(@categories.count-1)]
-    if @objects.count >= 6
-      @first_sixs_elements      = @objects[0..5]
-      if @objects.count >= 12
-        @second_three_elements  = @objects[5..7]
-      else
-        @second_three_elements  = @objects[5..(@categories.count-1)]
-      end
-    else
-      @first_sixs_elements      = @objects[0..(@categories.count-1)]
-      @second_three_elements    = nil
-    end
 
-    @random_outstanding_object = getRandomObject
   end
 
   def show
-    @category  = Category.find(params[:id])
-    @objects   = @category.restored_objects.page params[:page]
+    @categories  = Category.all
+    @objects     = RestoredObject.all
+    @category    = Category.find(params[:id])
+    @category_id = params[:id]
   end
 
   def getRandomObject
