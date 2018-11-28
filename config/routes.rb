6ccd86a72fc87265eec3a5ffd789bcd8429ed6f4
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-11-28 22:55:13 +0100

    Several UI fixes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
-    root 'categories#index'
+    root 'home#index'
   end
 end
