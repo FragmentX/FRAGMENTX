c83d5a2b6614ef3ee688aaefcbcb6cf1eb8024a8
duxy1996 <durancdr96@gmail.com>
2018-11-28 20:16:29 +0100

    [Issue #4]-Added avatar to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,17 @@ class UsersController < ApplicationController
 
   helper_method :isObjectFromUser
 
+  def create
+    @user = User.create(user_params)
+  end
+
+  private
+
+  # Use strong_parameters for attribute whitelisting
+  # Be sure to update your create() and update() controller methods.
+
+  def user_params
+    params.require(:user).permit(:avatar)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_170820) do
+ActiveRecord::Schema.define(version: 2018_11_28_185539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -162,6 +162,10 @@ ActiveRecord::Schema.define(version: 2018_11_28_170820) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.text "bio"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.bigint "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
